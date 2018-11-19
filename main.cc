1d332d988e9c85087a80be6924bb7e5088d7f96b
Conor Cunningham <ccunnin5@binghamton.edu>
2018-11-18 19:08:25 -0500

    Add storage for received messages and handling for rumor messages.

--- main.cc
+++ main.cc
@@ -1,13 +1,15 @@
 
 #include <unistd.h>
-
 #include <QVBoxLayout>
 #include <QApplication>
 #include <QDebug>
 #include <QDateTime>
-
 #include "main.hh"
 
+////////
+
+// Construct the ChatDialog window. 
+// Set the layout, the sequence number, and register necessary callbacks.
 ChatDialog::ChatDialog(NetSocket *s) {
   // Read-only text box where we display messages from everyone.
   // This widget expands both horizontally and vertically.
@@ -32,11 +34,14 @@ ChatDialog::ChatDialog(NetSocket *s) {
 
   sock = s;
 
+  // Set unique identifier
+  QString hostname = QHostInfo::localHostName();
   qsrand((uint) QDateTime::currentMSecsSinceEpoch());
-  myOrigin = QString::number(qrand());
+  myOrigin = hostname + QString::number(qrand());
   qDebug() << "myOrigin is " << myOrigin;
-	setWindowTitle("P2Papp - " + myOrigin);
+  setWindowTitle("P2Papp - " + myOrigin);
 
+  // Set sequence number beginning at 1
   mySeqNo = 1;
 
   // Register a callback on the textline's returnPressed signal
@@ -45,42 +50,81 @@ ChatDialog::ChatDialog(NetSocket *s) {
   connect(s, SIGNAL(readyRead()), this, SLOT(gotMessage()));
 }
 
+// Callback when user presses "Enter" in the textline widget.
+// Propogates rumor messages. 
 void ChatDialog::gotReturnPressed() {
-	// Initially, just echo the string locally.
-	// Insert some networking code here...
-	qDebug() << "FIX: send message to other peers: " << textline->text();
-  textview->append(textline->text());
+  QString message = textline->text();
 
-  sendRumorMessage(textline->text());
+  // Add the message to the chat window. 
+  textview->append(message);
 
-	// Clear the textline to get ready for the next input message.
-	textline->clear();
+  // Save the message.
+  saveMessage(myOrigin, mySeqNo, message);
+
+  // Send the rumor.
+  sendRumorMessage(myOrigin, mySeqNo, message);
+
+  // Update the sequence number.
+  mySeqNo++;
+
+  // Clear the textline to get ready for the next input message.
+  textline->clear();
 }
 
-void ChatDialog::sendRumorMessage(QString text) {
+// Save input sent the chat window. 
+void ChatDialog::saveMessage(QString origin, qint32 seq, QString text) {
+  // Check if we store this origin already.
+  if (originsMap.count(origin) > 0) {
+    // Add entry to the messages mapping.
+    originsMap.at(origin).insert(std::pair<qint32, QString>(seq, text));
+    // prettyPrintMaps();
+  } else {
+    // We have not seen this origin before - create a new messages map.
+    Messages messagesMap;
+    messagesMap.insert(std::pair<qint32, QString>(seq, text));
+
+    // Add the messages map to the corresponding origin
+    originsMap.insert(std::pair<QString, Messages>(origin, messagesMap));
+    // prettyPrintMaps();
+  }
+}
+
+// Serialize and propogate a rumor message.
+void ChatDialog::sendRumorMessage(QString origin, qint32 seq, QString text) {
+
   QByteArray buf;
   QDataStream datastream(&buf, QIODevice::ReadWrite);
   QVariantMap message;
 
+  // Serialize the message.
   message["ChatText"] = text;
-  message["Origin"] = myOrigin;
-  message["SeqNo"] = mySeqNo++;
+  message["Origin"] = origin;
+  message["SeqNo"] = seq;
+
+  qDebug() << "";
+  qDebug() << "Sending message to peers: "
+    << "<\"ChatText\",\"" << message["ChatText"].toString()
+    << "\"><\"Origin\",\"" << message["Origin"].toString()
+    << "\"><\"SeqNo\",\"" << message["SeqNo"].toString() << "\">";
+  qDebug() << "";
 
   datastream << message;
 
-  sock->writeDatagram(&buf);
-
+  // Send message to the socket. 
+  sock->writeDatagram(&buf, buf.size());
 }
 
+// Callback when receiving a message from the socket. 
 void ChatDialog::gotMessage() {
-  QVariantMap message;
   NetSocket *sock = this->sock;
 
+  // Read each datagram.
   while (sock->hasPendingDatagrams()) {
     QByteArray datagram;
     datagram.resize(sock->pendingDatagramSize());
     QHostAddress sender;
     quint16 senderPort;
+    QVariantMap message;
 
     sock->readDatagram(datagram.data(), datagram.size(), &sender, &senderPort);
 
@@ -89,46 +133,98 @@ void ChatDialog::gotMessage() {
     stream >> message;
 
     datagram.clear();
+
+    // Check if we have a status message or a rumor message.
+    if (message["Want"].isNull()) {
+      // Serialize the rumor message. 
+      QString mText = message["ChatText"].toString();
+      QString mOrigin = message["Origin"].toString();
+      qint32 mSeqNo = message["SeqNo"].toInt();
+      QString messageText = mText + " {" + mSeqNo + "@" + mOrigin + "}";
+
+      qDebug() << "";
+      qDebug() << "Received rumor: "
+        << "<\"ChatText\",\"" << mText
+        << "\"><\"Origin\",\"" << mOrigin
+        << "\"><\"SeqNo\",\"" << mSeqNo << "\">";
+      qDebug() << "";
+
+      // Check if we already have seen this message before.
+      if (originsMap.count(mOrigin) > 0 && originsMap.at(mOrigin).count(mSeqNo) > 0) {
+        if (originsMap.at(mOrigin).at(mSeqNo) != mText) {
+          qDebug() << originsMap.at(mOrigin).at(mSeqNo);
+          qDebug() << mText;
+          qDebug() << "Duplicate/corrupt message receivied. Ignoring";
+        } else {
+          qDebug() << "Duplicate message receivied. Ignoring";
+        }
+        continue;
+      }
+
+      // Save the message.
+      saveMessage(mOrigin, mSeqNo, mText);
+
+      // Send the rumor to neighbors.
+      sendRumorMessage(mOrigin, mSeqNo, mText);
+
+      // Display the message in the chat dialog window. 
+      textview->append(messageText);
+    // @TODO:
+    // - Respond to the status message.
+    } else {
+
+    }
   }
-
-  QString mText = message["ChatText"].toString();
-  QString mOrigin = message["Origin"].toString();
-  QString mSeqNo = message["SeqNo"].toString();
-  QString messageText = mText + " {" + mSeqNo + "@" + mOrigin + "}";
-  qDebug() << messageText;
-
-  textview->append(messageText);
-
 }
 
+// Helper function to print the originsMap and corresponding the messagesMap.
+void ChatDialog::prettyPrintMaps() {
+  for (std::map<QString,Messages>::iterator it=originsMap.begin(); it!=originsMap.end(); ++it) {
+    qDebug() << it->first;
+    for (std::map<qint32,QString>::iterator it2=it->second.begin(); it2!=it->second.end(); ++it2) {
+      qDebug() << it2->first;
+      qDebug() << it2->second;
+    }
+  }
+}
+
+////////
+
+// Constructor for the NetSocket class.
+// Define a range of UDP ports.
 NetSocket::NetSocket() {
-	// Pick a range of four UDP ports to try to allocate by default,
-	// computed based on my Unix user ID.
-	// This makes it trivial for up to four P2Papp instances per user
-	// to find each other on the same host,
-	// barring UDP port conflicts with other applications
-	// (which are quite possible).
-	// We use the range from 32768 to 49151 for this purpose.
-	myPortMin = 32768 + (getuid() % 4096)*4;
-	myPortMax = myPortMin + 3;
+  // Pick a range of four UDP ports to try to allocate by default,
+  // computed based on my Unix user ID.
+  // This makes it trivial for up to four P2Papp instances per user
+  // to find each other on the same host,
+  // barring UDP port conflicts with other applications
+  // (which are quite possible).
+  // We use the range from 32768 to 49151 for this purpose.
+  myPortMin = 32768 + (getuid() % 4096)*4;
+  myPortMax = myPortMin + 3;
   qDebug() << myPortMin << " - " << myPortMax;
 }
 
+// Bind the Netsocket to a port in a range of ports defined above.
 bool NetSocket::bind() {
-	// Try to bind to each of the range myPortMin..myPortMax in turn.
-	for (int p = myPortMin; p <= myPortMax; p++) {
-		if (QUdpSocket::bind(p)) {
-			qDebug() << "bound to UDP port " << p;
+  // Try to bind to each of the range myPortMin..myPortMax in turn.
+  for (int p = myPortMin; p <= myPortMax; p++) {
+    if (QUdpSocket::bind(p)) {
+      qDebug() << "bound to UDP port " << p;
       myPort = p;
-			return true;
-		}
-	}
+      return true;
+    }
+  }
 
-	qDebug() << "Oops, no ports in my default range " << myPortMin
-		<< "-" << myPortMax << " available";
-	return false;
+  qDebug() << "Oops, no ports in my default range " << myPortMin
+    << "-" << myPortMax << " available";
+  return false;
 }
 
+// Find the 2 closest (quickest responding) neighbors.
+// @TODO:
+// - Add timeouts to determine which neighbors are the fastest.
+// - If there are no neighbors, pick two random ports.
 QList<int> NetSocket::getAllNeighboringPorts() {
   QList<int> list;
   for (int i = myPortMin; i < myPortMax; i++) {
@@ -139,32 +235,39 @@ QList<int> NetSocket::getAllNeighboringPorts() {
   return list;
 }
 
-qint64 NetSocket::writeDatagram(QByteArray *buf) {
+// Propogate messages to all neighbors.
+qint64 NetSocket::writeDatagram(QByteArray *buf, int bufSize) {
+  qint64 bytesSent = 0;
+  qint64 totalBytesSent = 0;
   QList<int> neighbors = this->getAllNeighboringPorts();
-  qint64 bytesSent;
   for (int p : neighbors) {
-    if ((bytesSent = QUdpSocket::writeDatagram(*buf, QHostAddress::LocalHost, p)) < 0) {
-      return bytesSent;
+    bytesSent = QUdpSocket::writeDatagram(*buf, QHostAddress::LocalHost, p);
+    if (bytesSent < 0 || bytesSent != bufSize) {
+      qDebug() << "Error sending full datagram to " << QHostAddress::LocalHost << ":" << p << ".";
+    } else { 
+      totalBytesSent += bytesSent;
     }
-    bytesSent = 0;
   }
-  return bytesSent;
+  return totalBytesSent;
 }
 
+////////
+
 int main(int argc, char **argv) {
-	// Initialize Qt toolkit
-	QApplication app(argc,argv);
+  // Initialize Qt toolkit
+  QApplication app(argc,argv);
 
   // Create a UDP network socket
+  // NetSocket sock;
   NetSocket *sock = new NetSocket();
   if (!sock->bind())
     exit(1);
 
-	// Create an initial chat dialog window
-	ChatDialog *dialog = new ChatDialog(sock);
-	dialog->show();
+  // Create an initial chat dialog window
+  // ChatDialog dialog;
+  ChatDialog *dialog = new ChatDialog(sock);
+  dialog->show();
 
-	// Enter the Qt main loop; everything else is event driven
-	return app.exec();
+  // Enter the Qt main loop; everything else is event driven
+  return app.exec();
 }
-
--- main.hh
+++ main.hh
@@ -2,48 +2,65 @@
 #define P2PAPP_MAIN_HH
 
 #include <QDialog>
+#include <QTimer>
 #include <QTextEdit>
 #include <QLineEdit>
+#include <QHostInfo>
 #include <QUdpSocket>
+#include <map>
 
-class NetSocket : public QUdpSocket
-{
+#define TIMEOUT 1
+#define ANTI_ENTROPY_TIME 10
+
+////////
+
+class NetSocket : public QUdpSocket {
   Q_OBJECT
 
-public:
-  NetSocket();
+  public:
+    NetSocket();
+    // Bind this socket to a P2Papp-specific default port.
+    bool bind();
+    // Send data.
+    qint64 writeDatagram(QByteArray*, int);
+    QList<int> getAllNeighboringPorts();
 
-  // Bind this socket to a P2Papp-specific default port.
-  bool bind();
-  // send data
-  qint64 writeDatagram(QByteArray*);
-  QList<int> getAllNeighboringPorts();
-
-private:
-  int myPortMin;
-  int myPortMax;
-  int myPort;
+  private:
+    int myPortMin;
+    int myPortMax;
+    int myPort;
 };
 
+////////
 
 class ChatDialog : public QDialog {
-	Q_OBJECT
+  Q_OBJECT
 
-public:
-  NetSocket *sock;
-  QVariantMap allMessages;
-  ChatDialog(NetSocket*);
-  void sendRumorMessage(QString);
+  // Mapping of sequence numbers to messages.
+  typedef std::map<qint32, QString> Messages;
+  // Mapping of origins to a map of sequence numbers and messages.
+  typedef std::map<QString, Messages> Origins;
 
-public slots:
-	void gotReturnPressed();
-  void gotMessage();
+  public:
+    NetSocket *sock;
+    QVariantMap allMessages;
+    ChatDialog(NetSocket*);
+    void saveMessage(QString, qint32, QString);
+    void sendRumorMessage(QString, qint32, QString);
 
-private:
-	QTextEdit *textview;
-	QLineEdit *textline;
-  QString myOrigin;
-  qint32 mySeqNo;
+  public slots:
+    void gotReturnPressed();
+    void gotMessage();
+    void prettyPrintMaps();
+
+  private:
+    QTextEdit *textview;
+    QLineEdit *textline;
+    QString myOrigin;
+    qint32 mySeqNo;
+    Origins originsMap;
 };
+
+////////
 
 #endif // P2PAPP_MAIN_HH
